5a877ad0d5c07d66bd9a00944479c70f010baa7a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-10 03:45:11 -0700

    comment out install/uninstall for this release

--- commands/index.js
+++ commands/index.js
@@ -2,8 +2,8 @@ var order = [
   require('./addUser')
   , require('./restart')
   , require('./list')
-  , require('./install')
-  , require('./uninstall')
+  //, require('./install')
+  //, require('./uninstall')
   , require('./init')
   , require('./runTest')
 ]
